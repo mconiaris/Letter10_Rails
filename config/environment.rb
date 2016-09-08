1ad29036e6a2ec1d72cc65a7eb99109c4af0f679
mconiaris <mconiaris@gmail.com>
2016-09-08 08:37:00 -0400

    Configured sitemap generator.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configuration for the sitemap generator
+config.gem 'sitemap_generator'
